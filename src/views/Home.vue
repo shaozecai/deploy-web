1b56c2dba5cf709f1aa9267fa0927ee5b9a47600
shaozecai <admin@shaozecai.com>
2019-08-16 15:15:44 +0800

    recoding

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    {{ datas }}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+export default {
+  name: 'home',
+  data(){
+    return {
+       datas:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/user').then((response)=>{
+      this.datas = response.data
+    })
+  }
+}
+</script>
